38cd7ce35a225eb5345fb4f73c1f908d4b9afc55
oavaDev <omarvega598@gmail.com>
2023-03-15 12:51:44 -0500

    feat(Routes): add projects routes

---
+++ src/routes/projects.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/projects');
+router.get('/projects:id');
+router.post('/projects');
+router.put('/projects:id');
+router.delete('/projects:id');
+
+export default router;
