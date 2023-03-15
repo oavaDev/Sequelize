c8d3f7e0f33f107a46116fd99ab1b6806db41350
oavaDev <omarvega598@gmail.com>
2023-03-15 12:30:22 -0500

    feat(Model): added project model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import app from './app.js';
 import sequelize from './database/database.js';
-
+import './models/Projects.js';
 async function main() {
   try {
-    await sequelize.authenticate();
+    await sequelize.sync();
     console.log('Connection has been established successfully.');
     app.listen(3000);
     console.log('Server on port 3000');
